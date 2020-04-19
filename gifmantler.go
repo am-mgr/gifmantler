6e7b92c6834188bca062b45623fdc8d9445b621a
am-mgr <am.mgr.dev@gmail.com>
2020-04-19 14:43:57 +0530

    Use goroutine to write images

--- gifmantler.go
+++ gifmantler.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/gif"
 	"image/png"
 	"os"
 	"path"
 	"strconv"
+	"sync"
 )
 
 //ProcessGIF will unpack the frames to individual images
@@ -22,13 +24,22 @@ func ProcessGIF(filePath string) {
 		fmt.Println("Could not decode GIF")
 		return
 	}
+	var wg sync.WaitGroup
 	for id, img := range gifRef.Image {
 		outPath := path.Join(GetOutputPath(filePath), strconv.Itoa(id+1)) + ".png"
-		fs, err := os.OpenFile(outPath, os.O_CREATE, 0766)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer fs.Close()
-		png.Encode(fs, img)
+		go writeImage(outPath, img, &wg)
 	}
+	wg.Wait()
+}
+
+func writeImage(imgPath string, img image.Image, wg *sync.WaitGroup) {
+	wg.Add(1)
+	defer wg.Done()
+
+	fs, err := os.OpenFile(imgPath, os.O_CREATE, 0766)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer fs.Close()
+	png.Encode(fs, img)
 }
